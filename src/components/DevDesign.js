58bfa166b61b52e4ede82faa9825faf603e6a70f
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-21 09:04:41 -0500

    added images and some styling to the images underneath the main view

--- src/components/DevDesign.js
+++ src/components/DevDesign.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Reso from './images/image_of_reso.png';
 import Tend from './images/tend_screen.png';
+import vs from './images/ux_vs_ui.png';
 import './DevDesign.css';
 
 class DevDesign extends Component {
     render() {
         return(
+          <div>
             <h5>hello world</h5>
+            <img src={Reso} alt="screen shot of Reso by Meg Scholl" className="image-medium"/>
+            <p>this is reso</p>
+            <img src={Tend} alt="screen shot of Tend by Meg Scholl" className="image-medium"/>
+            <p>this is tend</p>
+            <img src={vs} alt="UX vs UI diagram" className="image-large"/>
+          </div>
         )
     }
 }
