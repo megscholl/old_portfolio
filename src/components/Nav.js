6e53d04136bcfc21a7ed458503c2399c0d48d08f
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-22 09:45:23 -0500

    added some images and added an inspirations section which incorporates my pinterest coding and design. also updated some styling and media queries

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ class Nav extends Component {
     render() {
         return(
             <nav>
-                <span href="#" className="links" alt="">design &amp; dev</span> &nbsp;&#124;&nbsp; <span href="#" className="links" alt="">about me</span> 
+                <span href="#designdev" className="links" alt="">design &amp; dev</span> &nbsp;&#124;&nbsp; <span href="#" className="links" alt="">about me</span> &nbsp;&#124;&nbsp; <span href="#inspirations" className="links" alt="">inspirations</span>
             </nav>
         );
     }
