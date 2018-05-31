5ab911174b207a100cd310ad8a63ea41f05f91fb
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-31 15:53:52 -0500

    added switch statement for hexagons to provide links

--- src/components/Inspirations.js
+++ src/components/Inspirations.js
@@ -1,37 +1,91 @@
 import React, { Component } from 'react';
 import './Hexagon.css';
+// import Pinterest from 'url(http://www.pinterest.com/iifge/coding-design)';
 // import { PinterestBoardWidget } from 'react-pinterest';
 
+// const NavClick = "";
+
 class Inspirations extends Component {
+
+    NavClick = (e) => {
+        let targetClass =  e.target.className;
+        let link = targetClass.substring(12, targetClass.length);
+
+        switch(link){
+            case "pinterest":
+                window.open("http://www.pinterest.com/iifge/coding-design", "_blank")
+                break;
+
+            case "pinspiry":
+                window.open("http://www.pinspiry.com", "_blank")
+                break;
+
+                
+            case "awwwards":
+                window.open("http://www.awwwards.com", "_blank")
+                break;
+
+                
+            case "dribbble":
+                window.open("http://www.dribbble.com/", "_blank")
+                break;
+
+                
+            case "medium":
+                window.open("http://www.medium.com", "_blank")
+                break;
+
+
+            case "behance":
+                window.open("http://www.behance.com", "_blank")
+                break;
+
+            default:
+                break;
+            
+        }
+
+        console.log("substring pinterest", link);
+        //transform this - use a string method to take'hexagon'className out and grab only the hex-[brand] to target the singlular hexagon below
+
+    }
+
     render() {
         return(
             <div>
                 <h5 className="header-center" id="inspirations">inspirations</h5>
                 <div className="inspirations-inline">
-                    <a href="http://www.pinterest.com/iifge/coding-design" target="_blank" rel="noopener noreferrer" className="links" alt="meg's pinterest board for coding and design"><div className="hexagon hex-pinterest">
-                        <div className="hexTop"></div>
-                        <div className="hexBottom"></div>
-                    </div></a>
-                    <div className="hexagon hex-pinsiry">
+
+                    <div onClick={ this.NavClick } className="hexagon hex-pinterest" alt="pinterest" title="pinterest" >
                         <div className="hexTop"></div>
                         <div className="hexBottom"></div>
                     </div>
-                    <div className="hexagon hex-awwwards">
+
+                    <div onClick={ this.NavClick } className="hexagon hex-pinspiry" alt="pinspiry" title="pinspiry" >
                         <div className="hexTop"></div>
                         <div className="hexBottom"></div>
                     </div>
-                    <div className="hexagon hex-dribbble">
+
+                    <div onClick={ this.NavClick } className="hexagon hex-awwwards" alt="awwwards" title="awwwards" >
                         <div className="hexTop"></div>
                         <div className="hexBottom"></div>
                     </div>
-                    <div className="hexagon hex-medium">
+
+                    <div onClick={ this.NavClick } className="hexagon hex-dribbble" alt="dribbble" title="dribbble" >
                         <div className="hexTop"></div>
                         <div className="hexBottom"></div>
                     </div>
-                    <div className="hexagon hex-behance">
+
+                    <div onClick={ this.NavClick } className="hexagon hex-medium" alt="medium" title="medium" >
                         <div className="hexTop"></div>
                         <div className="hexBottom"></div>
                     </div>
+
+                    <div onClick={ this.NavClick } className="hexagon hex-behance" alt="behance" title="behance" >
+                        <div className="hexTop"></div>
+                        <div className="hexBottom"></div>
+                    </div>
+
                 </div>
             </div>
         )
