41d12ac2e6f3b2febfe0f87e43c0e631d8549148
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-31 14:47:01 -0500

    added pinterest anchor tag to inspirations

--- src/components/Inspirations.js
+++ src/components/Inspirations.js
@@ -8,10 +8,10 @@ class Inspirations extends Component {
             <div>
                 <h5 className="header-center" id="inspirations">inspirations</h5>
                 <div className="inspirations-inline">
-                    <div className="hexagon hex-pinterest">
+                    <a href="http://www.pinterest.com/iifge/coding-design" target="_blank" rel="noopener noreferrer" className="links" alt="meg's pinterest board for coding and design"><div className="hexagon hex-pinterest">
                         <div className="hexTop"></div>
                         <div className="hexBottom"></div>
-                    </div>
+                    </div></a>
                     <div className="hexagon hex-pinsiry">
                         <div className="hexTop"></div>
                         <div className="hexBottom"></div>
