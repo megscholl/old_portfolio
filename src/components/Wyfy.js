c610e9868d2a1dd4e86800a296ea0883eb8da38f
Meg Scholl <meghan.e.scholl@gmail.com>
2018-06-06 14:22:47 -0500

    alt for wyfy image

--- src/components/Wyfy.js
+++ src/components/Wyfy.js
@@ -6,7 +6,7 @@ class Wyfy extends Component {
     render() {
         return(
             <div className="wyfy-project-width">
-                <a href="https://github.com/megscholl/capstone" alt="Reso, Meg Scholl NSS C24 Midway Capstone" title="Reso, Meg Scholl NSS C24 Midway Capstone" target="_blank" rel="noopener noreferrer"><img src={WyfyImg} alt="screen shot of Reso by Meg Scholl" className="wyfy-image zoom"/></a>
+                <img src={WyfyImg} alt="screen shot of NSS C24 Pitch Perfect WyFy app" className="wyfy-image zoom"/>
                 <p className="wrap-wyfy-text"><span className="drop-cap">M</span>y role with <strong>Wyfy</strong> was voted Team Leader in the most complex design and development project we'd ever had. Our Pitch Perfect presentation was to win the interest of four Tennessee State Professionals, ranging in IT to marketing research.
                 </p>
 
