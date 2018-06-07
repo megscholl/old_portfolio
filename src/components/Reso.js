972744ec0e42965986c856aa675078ecf3b9e072
Meg Scholl <meghan.e.scholl@gmail.com>
2018-06-07 12:49:15 -0500

    added reso html for deployed site

--- src/components/Reso.js
+++ src/components/Reso.js
@@ -7,7 +7,7 @@ class Reso extends Component {
     render() {
         return(
             <div className="project-width">
-                <a href="https://github.com/megscholl/capstone" alt="Reso, Meg Scholl NSS C24 Midway Capstone" title="Reso, Meg Scholl NSS C24 Midway Capstone" target="_blank" rel="noopener noreferrer"><img src={ResoImg} alt="screen shot of Reso by Meg Scholl" className="image-examples zoom"/></a>
+                <a href="http://megscholl.com/reso/" alt="Reso, Meg Scholl NSS C24 Midway Capstone" title="Reso, Meg Scholl NSS C24 Midway Capstone" target="_blank" rel="noopener noreferrer"><img src={ResoImg} alt="screen shot of Reso by Meg Scholl" className="image-examples zoom"/></a>
 
                 <ResoTechs />
                 <p className="wrap-text"><span className="drop-cap">C</span>reate, read, update and delete reservations to your favorite restaurants with <strong>Reso</strong>.
--- src/components/Wyfy.js
+++ src/components/Wyfy.js
@@ -7,7 +7,7 @@ class Wyfy extends Component {
         return(
             <div className="wyfy-project-width">
                 <img src={WyfyImg} alt="screen shot of NSS C24 Pitch Perfect WyFy app" className="wyfy-image zoom"/>
-                <p className="wrap-wyfy-text"><span className="drop-cap">M</span>y role with <strong>Wyfy</strong> was voted Team Leader in the most complex design and development project we'd ever had. Our Pitch Perfect presentation was to win the interest of four Tennessee State Professionals, ranging in IT to marketing research.
+                <p className="wrap-wyfy-text"><span className="drop-cap">M</span>y role with <strong>Wyfy</strong> was voted Team Leader in the most complex design and development project we'd ever had. Our Pitch Perfect presentation was to win the interest of four State of Tennessee Professionals, ranging in IT to marketing research.
                 </p>
 
             </div>
