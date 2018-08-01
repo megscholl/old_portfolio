b20765b0be9794833883146b919880de339d557e
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-01 08:34:27 -0500

    replaced h6 with h5 to give it the same look as the about me section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,7 @@ class Resume extends Component {
     render() {
         return(
         <div className="section-resume">
-            <h6 className="header-center-less">my experiences</h6>
+            <h5 className="header-center-less">my experiences</h5>
             <p className="work-columns">
             <span className="resume-header">Nashville Software School /</span> <span className="job">Apprentice UI UX Designer & Front-end Developer / Jan 2018 - present</span><br /><br />
             <span className="resume-header"n>BrickTop's / </span><span className="job">Server & Hostess / Aug 2011 - present</span><br /><br />
