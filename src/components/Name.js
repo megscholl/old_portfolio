8816482eee65afa37dd12d662db639986425ae3c
Meghan Scholl <meghan@Meghans-MacBook-Pro.local>
2019-10-08 11:47:06 -0500

    new font for construction

--- src/components/Name.js
+++ src/components/Name.js
@@ -10,7 +10,7 @@ class Name extends Component {
                 <h1 className="name">{this.props.name}</h1>
                 <h2 className="title">front-end developer & ux web designer</h2>
                 <Picture />
-                <h3 className="links construction">this website is under construction</h3>
+                <h3 className="construction">this website is under construction</h3>
                 <h6><strong>to contact or learn more about meg, visit <a href="https://www.linkedin.com/in/meg-scholl" title="meg scholl linked in" alt="meg scholl linked in" className="links" >her LinkedIn page</a></strong></h6>
                 
             </div>
